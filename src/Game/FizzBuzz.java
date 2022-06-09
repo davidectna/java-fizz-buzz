82ba239687b2b2a4101f8a8f9b28a7271730b0d9
davidectna@gmail.com <INDIRIZZO_E-MAIL>
2022-06-09 20:44:47 +0200

    finally commit

--- src/Game/FizzBuzz.java
+++ src/Game/FizzBuzz.java
@@ -2,6 +2,20 @@ package Game;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-		
+    	String multipleFive = "Buzz"; 
+    	String multipleThree = "Fizz";
+    	String multipleThreeAndFive = "FizzBuzz";
+    	
+    	
+    	for (int i = 1; i <= 100 ; i++) {
+   
+    		if(i % 3 == 0 && i % 5 == 0)  
+    			System.out.println(multipleThreeAndFive);
+    		else if (i % 3 == 0)
+    			System.out.println(multipleThree);
+    		else if (i % 5 == 0)
+    			System.out.println(multipleFive);
+    		else System.out.println(i);
+    	}
 	}
 }
